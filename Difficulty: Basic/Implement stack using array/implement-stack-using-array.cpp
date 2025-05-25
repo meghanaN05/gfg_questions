1d1476db58c44262f2288ad9cd622fb1fc4f18cc
Meghana N <meghananoochila@gmail.com>
2025-05-25 19:30:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.cpp
@@ -0,0 +1,28 @@
+// Function to push an integer into the stack.
+
+/*
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
+
+void MyStack ::push(int x) {
+    // Your Code
+    
+        arr[++top]=x;
+    
+}
+
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    // Your Code
+    if(top<0) return -1;
+    else return arr[top--];
+}
