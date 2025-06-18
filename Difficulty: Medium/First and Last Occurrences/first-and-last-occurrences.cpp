920255502970d4ad121453813c8a6ccc52ea3fa7
Meghana N <meghananoochila@gmail.com>
2025-06-18 22:16:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> find(vector<int>&nums, int x) {
+        // code here
+        auto lb = lower_bound(nums.begin(), nums.end(), x);
+        auto ub = upper_bound(nums.begin(), nums.end(), x);
+
+       
+        if (lb == nums.end() || *lb != x) return {-1, -1};
+
+        int first = lb - nums.begin();
+        int last = ub - nums.begin() - 1;
+
+        return {first, last};
+    }
+};
