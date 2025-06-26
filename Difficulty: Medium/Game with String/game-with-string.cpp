da33641fae3227938bc25127b76bfb14ce801136
Meghana N <meghananoochila@gmail.com>
2025-06-26 10:53:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Game with String/game-with-string.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int minValue(string &s, int k) {
+        vector<int> freq(26, 0);
+        for (char c : s)
+            freq[c - 'a']++;
+
+       
+        priority_queue<int> pq;
+        for (int f : freq) {
+            if (f > 0)
+                pq.push(f);
+        }
+
+        while (k-- && !pq.empty()) {
+            int top = pq.top();
+            pq.pop();
+            top--;
+            if (top > 0)
+                pq.push(top);
+        }
+
+        int sum = 0;
+        while (!pq.empty()) {
+            int f = pq.top(); pq.pop();
+            sum += f * f;
+        }
+
+        return sum;
+    }
+};
