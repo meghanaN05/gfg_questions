ae788f3b9a2facfb6d152bd8b228322aaede1964
Meghana N <meghananoochila@gmail.com>
2025-07-11 22:49:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int countConsec(int n) {
+        // code here
+        vector<int> dp0(n + 1, 0); // Ends with 0
+    vector<int> dp1(n + 1, 0); 
+
+    dp0[1] = 1; // "0"
+    dp1[1] = 1; // "1"
+
+    for (int i = 2; i <= n; i++) {
+        dp0[i] = dp0[i - 1] + dp1[i - 1];
+        dp1[i] = dp0[i - 1];
+    }
+
+    int total = pow(2, n);
+    int noConsecutiveOnes = dp0[n] + dp1[n];
+    return total - noConsecutiveOnes;
+        
+    }
+};
