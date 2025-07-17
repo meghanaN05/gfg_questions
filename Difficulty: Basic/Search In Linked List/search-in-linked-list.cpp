b3256b8978eae564b23ec3ec27f46602f5ccbc0c
Meghana N <meghananoochila@gmail.com>
2025-07-17 14:43:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Search In Linked List/search-in-linked-list.cpp
@@ -0,0 +1,27 @@
+// User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+
+};
+*/
+class Solution {
+  public:
+    bool searchLinkedList(Node *head, int x) {
+        // Your code here
+        Node *temp=head;
+        while(temp){
+            if(temp->data==x) return true;
+            temp=temp->next;
+        }
+        return false;
+    }
+};
