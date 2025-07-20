0efc05d0e02578e43fcc85ee97a92e79d7ee1109
Meghana N <meghananoochila@gmail.com>
2025-07-20 11:55:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse Array Using Stack/reverse-array-using-stack.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+
+// Complete this function
+class Solution {
+  public:
+    void reverseArray(vector<int>& arr) {
+   stack<int>st;
+   for(int i=0;i<arr.size();i++)
+   st.push(arr[i]);
+   for(int i=0;i<arr.size();i++){
+       arr[i]=st.top();
+       st.pop();
+   }
+    }
+};
