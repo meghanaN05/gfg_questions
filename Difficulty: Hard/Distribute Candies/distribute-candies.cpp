b1ec2dad97f12e671f28f4c6c9d0b8350b55b777
Meghana N <meghananoochila@gmail.com>
2025-10-12 22:45:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distribute Candies/distribute-candies.cpp
@@ -0,0 +1,32 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int moves = 0;
+
+    int dfs(Node* root) {
+        if (!root) return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        moves += abs(left) + abs(right);
+        return root->data + left + right - 1;
+    }
+    int distCandy(Node* root) {
+      moves = 0;
+        dfs(root);
+        return moves;
+        
+    }
+};
