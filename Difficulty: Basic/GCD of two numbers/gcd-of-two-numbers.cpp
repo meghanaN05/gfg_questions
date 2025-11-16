f807539ee4e8765fcdd146c010913113919539b2
Meghana N <meghananoochila@gmail.com>
2025-11-16 21:59:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        while(b!=0){
+            int r=a%b;
+            a=b;
+            b=r;
+            
+        }
+        return a;
+    }
+};
