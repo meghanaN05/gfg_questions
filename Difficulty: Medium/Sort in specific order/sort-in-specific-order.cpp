5d2984359281d3d9cfb4e9fa887aa58a6b7678e5
Meghana N <meghananoochila@gmail.com>
2025-12-18 16:17:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort in specific order/sort-in-specific-order.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void sortIt(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b) {
+            if (a % 2 != 0 && b % 2 != 0)
+                return a > b;
+            if (a % 2 == 0 && b % 2 == 0)
+                return a < b;
+            return (a % 2 != 0);
+        });
+        
+        
+    }
+};
