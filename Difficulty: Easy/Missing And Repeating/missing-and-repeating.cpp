eb815b5213003936bf12666156a88b239a9ea46b
Meghana N <meghananoochila@gmail.com>
2025-12-18 23:55:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -1,22 +1,47 @@
 class Solution {
   public:
-    vector<int> findTwoElement(vector<int>& arr) {
-        // code here
-        int as=(arr.size()+1)*arr.size()/2;
-        int sum=accumulate(arr.begin(),arr.end(),0);
-       sort(arr.begin(),arr.end());
-       int n=0;
-       for(int i=0;i<arr.size()-1;i++){
-           if(arr[i]==arr[i+1]){
-               n=arr[i];
-               break;
-           }
-       }
-       sum-=n;
-       vector<int>ans(2);
-       ans[0]=n;
-       ans[1]=as-sum;
-       return ans;
+    vector<int> findTwoElement(vector<int>&nums) {
+        int n = nums.size(); 
+        int xr = 0;
+        for (int i = 0; i < n; i++) {
+            xr = xr ^ nums[i]; 
+            xr = xr ^ (i + 1);  
+        }
+        int number = (xr & ~(xr - 1));
+        int zero = 0; 
+        int one = 0;  
+
+        for (int i = 0; i < n; i++) {
+            if ((nums[i] & number) != 0) {
+                one = one ^ nums[i];
+                
+            } else {
+                zero = zero ^ nums[i]; 
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & number) != 0) {
+                one = one ^ i; 
+                
+            } else {
+                zero = zero ^ i; 
+            }
+        }
+
+        int cnt = 0; 
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == zero) {
+                cnt++;
+            }
+        }
+
+        if (cnt == 2) {
+            return {zero, one}; 
+        }
+      
+        return {one, zero}; 
         
     }
 };
