90ff67596a12944ddfc4fb31862eed26a312a5c6
Meghana N <meghananoochila@gmail.com>
2025-12-26 19:22:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.cpp
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.cpp
@@ -1,21 +1,18 @@
-// User function template for C++
-
 class Solution {
   public:
     int kthMissing(vector<int> &arr, int k) {
-        // Your code goes here
-        int low=0;
-        int high=arr.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int missing=arr[mid]-(mid+1);
-            if(missing<k){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return k+1+high;
+      int l=0,r=arr.size()-1;
+      while(l<=r){
+          int mid=l+(r-l)/2;
+          int ans=arr[mid]-(mid+1);
+          if(ans<k){
+              l=mid+1;
+          }
+          else{
+              r=mid-1;
+          }
+      }
+      
+        return k+r+1;
     }
 };
