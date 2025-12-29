5bc31b900652854147b67e3691caaec4154c8de9
Meghana N <meghananoochila@gmail.com>
2025-12-29 09:44:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,33 +1,31 @@
 class Solution {
   public:
- 
-      int kthElement(vector<int>& a, vector<int>& b, int k) {
-    int n = a.size(), m = b.size();
-
-    if (n > m) return kthElement(b, a, k);
-    
-    int low = max(0, k - m), high = min(k, n);
-    
-    while (low <= high) {
-        int cut1 = (low + high) / 2;
-        int cut2 = k - cut1;
-        
-        int l1 = (cut1 == 0) ? INT_MIN : a[cut1 - 1];
-        int l2 = (cut2 == 0) ? INT_MIN : b[cut2 - 1];
-        int r1 = (cut1 == n) ? INT_MAX : a[cut1];
-        int r2 = (cut2 == m) ? INT_MAX : b[cut2];
-        
-        if (l1 <= r2 && l2 <= r1) {
-            return max(l1, l2);
-        } else if (l1 > r2) {
-            high = cut1 - 1;
-        } else {
-            low = cut1 + 1;
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        int m = a.size();
+        int n = b.size();
+        if (m > n) {
+            return kthElement(b, a, k); 
         }
-    }
-    
-    return -1;
-
+        int left = k; 
+        int low = max(0, k - n), high = min(k, m);
+        while (low <= high) {
+            int mid1 = (low + high)/2;
+            int mid2 = left - mid1;
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : INT_MIN;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 < m) ? a[mid1] : INT_MAX;
+            int r2 = (mid2 < n) ? b[mid2] : INT_MAX;
+            if (l1 <= r2 && l2 <= r1) {
+                return max(l1, l2);
+            } 
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            } 
+            else {
+                low = mid1 + 1;
+            }
+        }
+        return -1;
         
     }
 };
