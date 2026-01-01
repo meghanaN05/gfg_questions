785caa20d0cacc6f3be389620d1b62dc63a935c1
Meghana N <meghananoochila@gmail.com>
2026-01-01 12:33:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find length of Loop/find-length-of-loop.cpp
+++ Difficulty: Medium/Find length of Loop/find-length-of-loop.cpp
@@ -14,29 +14,26 @@ class Node {
 class Solution {
   public:
     int lengthOfLoop(Node *head) {
-        // code here
-        if (!head || !head->next) return 0;
-
-        Node *slow = head, *fast = head;
-
-        // Step 1: Detect cycle
+        if (!head) return 0;
+        Node *slow = head;
+        Node *fast = head;
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
 
             if (slow == fast) {
-                // Step 2: Count cycle length
                 int count = 1;
-                Node* temp = slow->next;
-                while (temp != slow) {
+                fast = fast->next;
+
+                while (fast != slow) {
                     count++;
-                    temp = temp->next;
+                    fast = fast->next;
                 }
                 return count;
             }
         }
-        // Step 3: No cycle
-        return 0;
-
+        return 0; 
     }
 };
+
+    
