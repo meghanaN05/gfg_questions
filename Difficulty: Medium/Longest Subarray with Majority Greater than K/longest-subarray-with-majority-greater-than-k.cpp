d3585a5090642f8f51b5918edbc81332175b7ad1
Meghana N <meghananoochila@gmail.com>
2026-02-25 11:19:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
@@ -1,28 +1,24 @@
 class Solution {
   public:
     int longestSubarray(vector<int> &arr, int k) {
-          int n = arr.size();
-    unordered_map<int, int> firstIndex;
-    int prefixSum = 0, maxLen = 0;
-
-    firstIndex[0] = -1;
-
-    for (int i = 0; i < n; ++i) {
-        int val = (arr[i] > k) ? 1 : -1;
-        prefixSum += val;
-
-        if (prefixSum > 0) {
-            maxLen = i + 1;
-        } else if (firstIndex.find(prefixSum - 1) != firstIndex.end()) {
-            maxLen = max(maxLen, i - firstIndex[prefixSum - 1]);
+        unordered_map<int,int>mp;
+        int maxi=0,sum=0;
+        for(int i=0;i<arr.size();i++){
+         if(arr[i]<=k) sum--;
+         else sum++;
+         if(sum>0) maxi=i+1;
+        else
+        {
+            if (mp.find(sum - 1) != mp.end()) {
+                maxi = max(maxi, i - mp[sum - 1]);
+            }
         }
-
-        if (firstIndex.find(prefixSum) == firstIndex.end()) {
-            firstIndex[prefixSum] = i;
+        if(mp.find(sum)==mp.end()){
+            mp[sum]=i;
         }
-    }
-
-    return maxLen;
+            
+        }
+        return maxi;
         
     }
 };
