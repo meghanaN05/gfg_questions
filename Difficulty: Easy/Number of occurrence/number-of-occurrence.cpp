aedf0d5df586b36671618b88fc561d6bd04bf7e1
Meghana N <meghananoochila@gmail.com>
2026-03-03 11:52:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,16 +1,11 @@
 class Solution {
   public:
-    int countFreq(vector<int>& nums, int x) {
+    int countFreq(vector<int>& arr, int target) {
         // code here
-          auto lb = lower_bound(nums.begin(), nums.end(), x);
-        auto ub = upper_bound(nums.begin(), nums.end(), x);
-
-       
-        if (lb == nums.end() || *lb != x) return 0;
-
-        int first = lb - nums.begin();
-        int last = ub - nums.begin() - 1;
-
-        return last-first+1;
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        return mp[target];
     }
 };
