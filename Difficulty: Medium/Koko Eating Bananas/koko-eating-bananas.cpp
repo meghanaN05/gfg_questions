9922e23727db81708b3e5bbab38bb6598e53e28f
Meghana N <meghananoochila@gmail.com>
2026-03-04 17:19:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
@@ -1,28 +1,26 @@
 class Solution {
   public:
-  bool checker(int mid,vector<int>& arr,int k){
-      long long hours=0;
-      for(int pile:arr){
-          hours+=(pile+mid-1)/mid;
+  bool possible( int mid,vector<int>& arr, int k){
+      int ans=0;
+      for(int i=0;i<arr.size();i++){
+          ans+=(arr[i] + mid - 1) / mid;
       }
-      return hours<=k;
+      return ans<=k;
   }
     int kokoEat(vector<int>& arr, int k) {
-        // Code here
-        int l=1;
-        int r=*max_element(arr.begin(),arr.end());
-        int ans=r;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(checker(mid,arr,k)){
-                ans=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
+       int l=1,r=*max_element(arr.begin(),arr.end());
+       int ans=-1;
+       while(l<=r){
+           int mid=l+(r-l)/2;
+           if(possible(mid,arr,k)){
+               ans=mid;
+               r=mid-1;
+           }
+           else{
+               l=mid+1;
+           }
+       }
+       return ans;
         
     }
 };
