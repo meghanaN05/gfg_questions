3d90d9bc9aea06f46359a54118a3c691eeb37037
Meghana N <meghananoochila@gmail.com>
2026-03-12 18:51:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,22 +1,18 @@
 class Solution {
-public:
+  public:
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
-        vector<int> ans;
-        int maxi = 0;
-        for (int i = 0; i < k; i++)
-            maxi = max(maxi, arr[i]);
-        ans.push_back(maxi);
-
-        for (int i = k; i < arr.size(); i++) {
-            if (arr[i - k] == maxi) {
-                maxi = arr[i - k + 1];
-                for (int j = i - k + 1; j <= i; j++)
-                    maxi = max(maxi, arr[j]);
-            } else {
-                maxi = max(maxi, arr[i]);
-            }
-            ans.push_back(maxi);
+        vector<int>ans;
+        multiset<int>mt;
+        for(int i=0;i<k;i++){
+            mt.insert(arr[i]);
+        }
+        ans.push_back(*mt.rbegin());
+        for(int i=k;i<arr.size();i++){
+            mt.erase(mt.find(arr[i-k]));
+            mt.insert(arr[i]);
+            ans.push_back(*mt.rbegin());
         }
         return ans;
+        
     }
 };
