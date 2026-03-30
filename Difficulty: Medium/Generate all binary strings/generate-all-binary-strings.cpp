aab08674934120ed93e371edfc6bfb9f568afb2a
Meghana N <meghananoochila@gmail.com>
2026-03-30 23:01:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
@@ -1,23 +1,17 @@
 class Solution {
   public:
-  void helper(int index, int n, string current, vector<string>& result, char prev) {
-      if (current.size() == n) {
-          result.push_back(current);
+  vector<string>ans;
+  void generate(int n,string s){
+      if(s.size()==n){
+          ans.push_back(s);
           return;
       }
-      
-     
-      helper(index + 1, n, current + '0', result, '0');
-      
-      
-      if (prev != '1') {
-          helper(index + 1, n, current + '1', result, '1');
-      }
+      generate(n,s+"0");
+      generate(n,s+"1");
   }
-
-  vector<string> generateBinaryStrings(int num) {
-      vector<string> result;
-      helper(0, num, "", result, '0');
-      return result;
-  }
+    vector<string> binstr(int n) {
+       generate(n,"");
+       return ans;
+        
+    }
 };
