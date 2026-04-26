9de8dda4c9de46e18ff25cf6e5515cd5aa9fa1ef
Meghana N <meghananoochila@gmail.com>
2026-04-27 00:05:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.cpp
@@ -0,0 +1,31 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void helper(Node* root, vector<int>& ans) {
+        if (root == NULL) return;
+        
+        helper(root->left, ans);  
+        ans.push_back(root->data);
+        helper(root->right, ans);  
+    }
+    
+    vector<int> inOrder(Node* root) {
+        vector<int> ans;
+        helper(root, ans);
+        return ans;
+    }
+};
