35d9ba12573c2632e846b3b78e4a41f4dccb8cd1
Meghana N <meghananoochila@gmail.com>
2026-04-29 23:37:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
+++ Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
@@ -1,32 +1,30 @@
-// User function Template for C++
-
-/* Tree Node
-struct Node {
+/*
+class Node {
+  public:
     int data;
     Node* left;
     Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
 };
 */
 class Solution {
   public:
     vector<int> preOrder(Node* root) {
-        // code here
-    vector<int>ans;
-    if(root==NULL) 
-    return ans;
     stack<Node*>st;
-    st.push(root);
-    while(!st.empty()){
-        root=st.top();
-        st.pop();
-        ans.push_back(root->data);
-        if(root->right)
-        st.push(root->right);
-        if(root->left)
-        st.push(root->left);
-        
-    }
-    return ans;
-        
+    vector<int>ans;
+    if(root==nullptr) return ans;
+        st.push(root);
+        while(!st.empty()){
+            Node* curr=st.top();
+            st.pop();
+            ans.push_back(curr->data);
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+        }
+        return ans;
     }
 };
